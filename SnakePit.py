bcbfbab0a750f31c5f9f28a4414dacf21f0a873e
Fish329 <268188@burlcoschools.org>
2025-03-06 08:27:02 -0500

    Update SnakePit.py

--- SnakePit.py
+++ SnakePit.py
@@ -21,7 +21,8 @@ class level:
                         else:
                             Previous.print_children()
                     elif pickOne==int:
-                        self.children[pickOne].print_children()
+                        stuff[self.children[pickOne]].print_children()
 testlvl2=level("testlvl2",[],"This is test level 2")
 testlvl1=level("testlvl1",["testlvl2"],"")
+stuff = {"testlvl1":testlvl1,"testlvl2":testlvl2}
 testlvl1.print_children() #TODO: figure out if this works
